2709811989f208951e91939c0b102ddcfad00000
pop <petarpopovic719@gmail.com>
2024-02-26 08:55:18 +0100

    novo

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -24,16 +24,21 @@ export function Navigation() {
     <div className="w-full h-16 flex justify-center items-center gap-4">
       {items.map((item) => {
         return (
-          <Link
-            className={`transition-all hover:underline ${
-              path === item.href ? "font-semibold underline text-lg" : ""
-            }`}
-            href={item.href}
+          <button
+            className="p-1 transition-all duration-100 ease-in-out rounded
+            hover:bg-zinc-700 hover:border-b-4 hover:border-t-2 hover:border-white hover:border-l-2 hover:border-r-2
+            active:transform active:border-b-0 active:translate-y-0"
             key={item.href}
           >
-            {" "}
-            {item.title}{" "}
-          </Link>
+            <Link
+                className={`transition-all duration-100 ease-in-out transform hover:-translate-y-2 ${
+                    path === item.href ? "font-semibold underline text-lg" : ""
+                }`}
+              href={item.href}
+            >
+              {item.title}
+            </Link>
+          </button>
         );
       })}
     </div>
