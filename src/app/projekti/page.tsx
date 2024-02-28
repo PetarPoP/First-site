cefe0a0bec39f94d4dce0339c6875cd6199c0c24
pop <petarpopovic719@gmail.com>
2024-02-28 11:01:18 +0100

    novo

--- src/app/projekti/page.tsx
+++ src/app/projekti/page.tsx
@@ -17,7 +17,7 @@ export default async function Page() {
 function Projekt({ projekat }: Readonly<{ projekat: Repository }>) {
   return (
     <div className="flex-col flex gap-2 p-2 rounded border-b-2">
-      <Link href={projekat.html_url} target="_blank" rel="noopener noreferrer" className="text-md">
+      <Link href={projekat.html_url} target="_blank" rel="noopener noreferrer" className="text-xl">
         <button className="p-1 transition-all duration-100 ease-in-out rounded border-b-2
                   hover:bg-zinc-700 hover:border-b-4 hover:border-t-2 hover:border-white hover:border-l-2 hover:border-r-2
                   active:transform active:border-b-0 active:translate-y-0">
